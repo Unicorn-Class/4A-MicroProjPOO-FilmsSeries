060ca1e4ac70e86edecda09e85bfd6ff8de4b167
Axel M. (amacz13) <axel.maczkowiak@live.fr>
2019-01-09 15:45:38 +0100

    Created Project

---
+++ src/main/java/fr/unicornteam/uniflix/UniflixApplication.java
@@ -0,0 +1,14 @@
+package fr.unicornteam.uniflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniflixApplication.class, args);
+	}
+
+}
+
