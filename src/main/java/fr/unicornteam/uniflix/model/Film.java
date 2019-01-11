d0d5366a40504d5737cbc644473f480cc944dd9b
AfuaDOM <sandradomyes@gmail.com>
2019-01-11 11:02:04 +0100

    film

--- src/main/java/fr/unicornteam/uniflix/model/Film.java
+++ src/main/java/fr/unicornteam/uniflix/model/Film.java
@@ -1,5 +1,14 @@
 package fr.unicornteam.uniflix.model;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 public class Film extends Media {
@@ -18,6 +27,51 @@ public class Film extends Media {
     }
 
 
+
+
+    ArrayList<Film> listeMovie=new ArrayList<Film>();
+    public Film(String keyMovie) throws ParseException, UnirestException {
+
+        String key="8600861f4787df9fb2f5752da938b459";
+        //HttpResponse<JsonNode> response = Unirest.get("https://api.themoviedb.org/3/search/movie?api_key="+key+"=fr-fr&query="+keyMovie).asJson();
+        HttpResponse<JsonNode> response = Unirest.get("https://api.themoviedb.org/3/search/movie?api_key="+key+"&language=en-US&query="+keyMovie).asJson();
+        JSONArray rs =response.getBody().getObject().getJSONArray("results");
+
+        this.id=rs.getJSONObject(0).getInt("id");
+        this.title=rs.getJSONObject(0).getString("original_title");
+        String date=rs.getJSONObject(0).getString("release_date");
+        this.release=new SimpleDateFormat("yyyy-MM-dd").parse(date);
+        this.overview=rs.getJSONObject(0).getString("overview");
+
+
+        HttpResponse<JsonNode> additionnal = Unirest.get("https://api.themoviedb.org/3/movie/"+this.id+"?api_key="+key).asJson();
+        JSONObject res2 =additionnal.getBody().getObject();
+        System.out.println(res2);
+
+        ArrayList<String> t=new ArrayList<String>();
+        for (int i =0;i<res2.getJSONArray("genres").length();i++){
+            t.add(res2.getJSONArray("genres").getJSONObject(i).getString("name"));
+        }
+
+        this.setType(t);
+        //System.out.println(res2.getJSONArray("genres").getJSONObject(0).getString("name"));
+        this.origin_country=res2.getJSONArray("production_countries").getJSONObject(0).getString("name");
+        //this.actor=new ArrayList<String>();
+
+
+        HttpResponse<JsonNode> ensemble=Unirest.get("https://api.themoviedb.org/3/movie/"+this.id+"/credits?api_key="+key).asJson();
+        JSONObject ens=ensemble.getBody().getObject();
+
+        ArrayList<String> act=new ArrayList<String>();
+
+
+        for (int i =0;i<ens.getJSONArray("cast").length();i++){
+            act.add(res2.getJSONArray("cast").getJSONObject(i).getString("name"));
+        }
+        this.setActor(act);
+
+    }
+
     /** Film(String keyword){
      String key ="8600861f4787df9fb2f5752da938b459";
      HttpResponse<String> response = Unirest.get("https://api.themoviedb.org/3/search/movie?api_key="+key+"=fr-fr&query="+keyword)
@@ -25,4 +79,29 @@ public class Film extends Media {
      .asString();
      System.out.println(response);
      }**/
+
+    public String toString() {
+        return "Serie{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", release=" + release +
+
+
+
+                ", origin_country='" + origin_country + '\'' +
+                ", overview='" + overview + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        try {
+            Film s=new Film("BLACK");
+            System.out.println(s);
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
