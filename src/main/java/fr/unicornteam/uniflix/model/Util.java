50f7fddbc5497ac4c4a28ef554ae2f1760db44b4
Nicolas <nack714@hotmail.fr>
2019-01-15 17:01:43 +0100

    Create class Util
    with getAl[...] & convertDate

---
+++ src/main/java/fr/unicornteam/uniflix/model/Util.java
@@ -0,0 +1,42 @@
+package fr.unicornteam.uniflix.model;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+
+public final class Util {
+
+    public static ArrayList<Media> allMedia = new ArrayList<>();
+
+    public static String convertDate(String str){
+        try {
+
+            SimpleDateFormat myDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+            DateFormat dateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy", Locale.US);
+            dateFormat.parse(str);
+            Date d = new Date();
+            dateFormat.format(d);
+            return myDateFormat.format(d);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static ArrayList<Media> allMedia(){
+        return allMedia;
+    }
+
+    public static ArrayList<Media> allSerie(){
+        return allMedia;
+    }
+
+    public static ArrayList<Media> allMovie(){
+        return allMedia;
+    }
+
+}
