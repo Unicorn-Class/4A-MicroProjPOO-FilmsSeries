e2e0a0a2c340ee7895b763159d610dfda67ef45c
Axel Maczkowiak <axel.maczkowiak@live.fr>
2019-01-18 14:13:33 +0100

    Updated App & Search

--- src/main/java/fr/unicornteam/uniflix/controller/AppController.java
+++ src/main/java/fr/unicornteam/uniflix/controller/AppController.java
@@ -1,11 +1,6 @@
 package fr.unicornteam.uniflix.controller;
 
-import fr.unicornteam.uniflix.model.Media;
 import fr.unicornteam.uniflix.model.Util;
-import fr.unicornteam.uniflix.repositories.MediaRepository;
-import fr.unicornteam.uniflix.repositories.MovieRepository;
-import fr.unicornteam.uniflix.repositories.SerieRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
