1c67ee5181b82231ff394b0f2fde769fe9a12202
Axel Maczkowiak <axel.maczkowiak@live.fr>
2019-01-18 15:47:54 +0100

    Updated Navbar & Controller

--- src/main/java/fr/unicornteam/uniflix/controller/MovieController.java
+++ src/main/java/fr/unicornteam/uniflix/controller/MovieController.java
@@ -10,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+
 @Controller
 public class MovieController {
     @Autowired
@@ -21,6 +23,12 @@ public class MovieController {
         Movie movie = null;
         for (Movie m : Util.allMovie()) {
             if (m.getId() == id) movie = m;
+        }
+        if (movie == null) return "app";
+        if (movie.getExtract() == null || movie.getExtract().size() == 0) {
+            ArrayList<String> ex = new ArrayList<>();
+            ex.add("No extract available for this title...");
+            movie.setExtract(ex);
         }
         model.addAttribute("movie", movie);
         return "movie";
